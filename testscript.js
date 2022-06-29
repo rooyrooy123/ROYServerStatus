dbd62bde0ecd595b6c49a2cd7d67d45faa33118a
rooyrooy123 <rooyrooytest@gmail.com>
2022-06-30 00:09:19 +0900

    made changes

--- testscript.js
+++ testscript.js
@@ -50,7 +50,7 @@ var ip = '123.198.130.10'
 
 let wDisplay = document.getElementById("idDisplay");
 let sCharset = "UTF-8";
-let sFileName = "replace.txt"
+let sFileName = "replacetest.txt"
 
 const timer = 5000    // ミリ秒で間隔の時間を指定
 window.addEventListener('load',function(){
@@ -74,41 +74,32 @@ if (this.readyState === 4 && this.status === 200) {
 
 function load(){
 
-
     var loopnumsplit = res.split('/')
-    var loopnum = loopnumsplit.length - 1;
+    var loopnum = loopnumsplit.length;
     //OPENのとこを色変更
     //行とりだす ループ0回目からでn
     for (var count = 0; count < loopnum; count++) {
-      var namesplit = res.split('/')
-      var name = numsplit[2*count];
+      console.log('loop:' +count)
+      var split = res.split('/')
+      var name = split[2*count];
       console.log(name)
-      var numsplit = res.split(':)')
-      var num = numsplit[count];
-      
-      
-      //openorclose 1 4 7  n=0からの等差数列3n + 1
-      var opensplit = res.split(':')
-      var open = opensplit[3*count + 1];
-
-      //port 1 2 3 4 5 ループ0回目からでn+1
-      portsplit = res.split('Port番号「');
-      var port = portsplit[count + 1]//変更するとここ2つはめ変更しないで
-      port = port.split('番」')
-      port = port[0]//変更しない
+      var port = split[2*count+1];
+      console.log(port)
 
 
+      var ul = document.getElementById('serverlist');
       // li要素を作成
       var li = document.createElement('li');
 
+
       // テキスト情報を作成
-      var info = document.createTextNode(num);
+      var info = document.createTextNode(name);
       
       // ul要素に追加
       li.appendChild(info);
 
 
-      if ((~name.indexOf('人狼RPG'))){
+      if (~name.indexOf('人狼RPG')){
         var version = document.createTextNode('version:1.12.2');
         li.appendChild(version);
       }else if(~name.indexOf('青鬼')){
@@ -131,54 +122,76 @@ function load(){
         var version = document.createTextNode('version:1.12.2');
         li.appendChild(version);
       }
+      var port2 = document.createTextNode(port);
+      li.appendChild(port2);
       ul.appendChild(li);  
 
-      
-
-      if (open == 'Closed'){//サーバークローズの場合
-        li.classList.add('serverclose');
-        li.classList.remove('serveropen');
-        var close = document.createTextNode('  ✘');
-        li.appendChild(close)
-        li.onclick=(copy2)
-      }else{//サーバー開放中の場合
-        li.classList.remove('serverclose');
-        li.classList.add('serveropen');
-        var opencircle = document.createTextNode('  〇');
-        li.appendChild(opencircle)
-        li.onclick=(copy2)
-        //以下開放中のサーバーリスト処理
-        var numsplit = res.split(':)')
-        var num = numsplit[count];
+      const url = 'http://nyatla.jp/ws/mcsapi/mcsapi.php?cmd=si&s=123.198.130.10&p=' + port;
+      fetch(url).then(function(response) {
         
-        var ul = document.getElementById('openserverlist');
-        // li要素を作成
+        return response.text();
+      }).then(function(text) {
         var li = document.createElement('li');
+        if(~text.indexOf('ONLINE')){
 
-        // テキスト情報を作成
-        var nameinfo = document.createTextNode(name);
-        var portinfo = document.createTextNode(':'+ port);
-        var info1 = document.createTextNode(':IP⇒');
-        var infoip = document.createTextNode(ip);
-        var namesplit = res.split(')')
-        var name = namesplit[2*count]
-
-        // ul要素に追加
-        var kuuhaku = document.createTextNode('   ');
-        li.appendChild(nameinfo);
-        li.appendChild(info1);
-        li.appendChild(infoip);
-        li.appendChild(portinfo);
-        ul.appendChild(li);
-        li.appendChild(kuuhaku);
-        li.appendChild(version);
-        li.onclick=(copy)
-
-
+          var check ='online'
       
+        }else{//サーバー閉鎖中の場合
+          var check ='offline'
+
+          var li = document.createElement('li');
+          li.classList.add('serverclose');
+          li.classList.remove('serveropen');
+          var close = document.createTextNode('  ✘');
+          li.appendChild(close)
+          li.onclick=(copy2)
+
+        }
+      if (check == 'online'){
+li.classList.remove('serverclose');
+          li.classList.add('serveropen');
+          var opencircle = document.createTextNode('  〇');
+          li.appendChild(opencircle)
+          li.onclick=(copy2)
+          //以下開放中のサーバーリスト処理
 
 
-       }
+          
+          var ul = document.getElementById('openserverlist');
+          // li要素を作成
+          var li = document.createElement('li');
+
+          // テキスト情報を作成
+          
+          var split = res.split('/')
+          var name = split[2*count];
+          var port = split[2*count+1];
+          
+          var nameinfo = document.createTextNode(name);
+          var portinfo = document.createTextNode(':'+ port);
+          var info1 = document.createTextNode(':IP⇒');
+          var infoip = document.createTextNode(ip);
+          var namesplit = res.split(')')
+          var name = namesplit[2*count]
+
+          // ul要素に追加
+          var kuuhaku = document.createTextNode('   ');
+          li.appendChild(nameinfo);
+          li.appendChild(info1);
+          li.appendChild(infoip);
+          li.appendChild(portinfo);
+          ul.appendChild(li);
+          li.appendChild(kuuhaku);
+          li.appendChild(version);
+          li.onclick=(copy)
+      }else{
+
+      }
+
+        
+
+      });
+       
     }
   }
 
