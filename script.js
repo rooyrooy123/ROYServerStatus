379695b1d239e19f39c3b4416b3a91ee3ba5b18f
rooyrooy123 <80926982+rooyrooy123@users.noreply.github.com>
2023-01-12 14:47:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -68,8 +68,8 @@ for (let i = 0; i < 10; ++i) {
     version.push(" Version:未 ")
   }else if(i == 3){
     port.push("30000");
-    server.push("サバイバル");
-    version.push(" Version:1.16.1 ")
+    server.push("TUSB v13α");
+    version.push(" Version:1.17.1 ")
   }else if(i == 4){
     port.push("40000");
     server.push("クリエイティブ");
